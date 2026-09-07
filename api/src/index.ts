80dcb1ab8bb49be0715431a461779f9be0c31152
Mohammed Akram <nerddude9000@proton.me>
2026-09-07 18:53:49 +0100

    schema: is_published

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,18 +1,26 @@
+import "dotenv/config";
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { csrf } from "hono/csrf";
 
 const app = new Hono();
 
+app.use(
+	cors({ origin: process.env.ORIGIN }),
+	csrf({ origin: process.env.ORIGIN }),
+);
+
 app.get("/", (c) => {
-  return c.text("Hello Hono!");
+	return c.text("Hello Hono!");
 });
 
 serve(
-  {
-    fetch: app.fetch,
-    port: 3000,
-  },
-  (info) => {
-    console.log(`Server is running on http://localhost:${info.port}`);
-  },
+	{
+		fetch: app.fetch,
+		port: 3000,
+	},
+	(info) => {
+		console.log(`Server is running on http://localhost:${info.port}`);
+	},
 );
