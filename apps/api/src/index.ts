4efdbe14374ba0b5765b1b140817d469beb31fa5
Mohammed Akram <nerddude9000@proton.me>
2026-09-09 01:37:48 +0100

    fix: graceful shutdown of node server

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -16,7 +16,7 @@ app.use(
 app.route("/auth", auth);
 app.route("/api/posts", posts);
 
-serve(
+const server = serve(
 	{
 		fetch: app.fetch,
 		port: 3000,
@@ -25,3 +25,18 @@ serve(
 		console.log(`Server is running on http://localhost:${info.port}`);
 	},
 );
+
+// graceful shutdown
+process.on("SIGINT", () => {
+	server.close();
+	process.exit(0);
+});
+process.on("SIGTERM", () => {
+	server.close((err) => {
+		if (err) {
+			console.error(err);
+			process.exit(1);
+		}
+		process.exit(0);
+	});
+});
